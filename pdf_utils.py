4d9301c5a72eb3434b8f8a7b99113297d434eb76
andreatsilva <andre.emidio09@outlook.pt>
2024-10-25 22:35:50 +0100

    Merge PDF Function

--- pdf_utils.py
+++ pdf_utils.py
@@ -1,3 +1,14 @@
 from PyPDF2 import PdfReader, PdfWriter
 from PIL import Image
 import os
+
+def mergepdf(pdf_list, output_path):
+    writer = PdfWriter()
+    for pdf in pdf_list:
+        reader = PdfReader()
+        for page in range(len(reader.pages)):
+            writer.add_page(reader.pages[page])
+    with open(output_path, 'wb') as output_pdf:
+        writer.write(output_pdf)
+    print(f"Merged PDF saved to {output_path}")
+
